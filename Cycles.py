df3ed54b49750f856cfe82cadbb7008164a94191
Lady777D <ladydevil777@gmail.com>
2025-09-26 13:31:07 +0300

    cycl

---
+++ Cycles.py
@@ -0,0 +1,30 @@
+# повторить много раз
+from inspect import isfunction
+
+for i in range(4):
+    print('Привет')
+
+n = 7
+for _ in range(n):
+    print('gav')
+
+x_wall = 120
+x_person = 10
+
+while x_person < x_wall:
+    x_person = x_person + 5
+print('хлоп')
+
+summa = 0
+while summa < 100:
+    namber = int(input('введите число'))
+    summa += namber
+print("спасибо")
+print( summa )
+
+# nam1 = int(input('положительное число'))
+# nam2 = int(input('отрицательное число'))
+# print(nam1 sep nam2)
+
+
+
